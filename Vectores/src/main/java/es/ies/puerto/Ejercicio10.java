911c25a8b0d87b4fb3ebd07bdd61f6616ae6d8e4
nexphernandez <nicolaseh3b1112@gmail.com>
2024-12-12 09:41:09 +0000

    Se realizan todos los ejercicios

--- Vectores/src/main/java/es/ies/puerto/Ejercicio10.java
+++ Vectores/src/main/java/es/ies/puerto/Ejercicio10.java
@@ -1,4 +1,9 @@
 package es.ies.puerto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * @author nexphernandez
  * @version 1.0.0
@@ -9,7 +14,21 @@ package es.ies.puerto;
  * Resultado esperado: Calificaciones: [80.0, 90.0, 70.0]. Promedio: 80.0.
  */
 public class Ejercicio10 {
+    static Vector<Float> numeros = new Vector<>(Arrays.asList(80.0f, 90.0f, 70.0f));
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(calcularPromedio(numeros));
+    }
+    /**
+     * Funcion para calcular el promedio de numero en una lista
+     * @param lista a calcular el promedio
+     * @return promedio
+     */
+    public static float calcularPromedio(List<Float>lista){
+        float suma = 0;
+        for (float numero : lista) {
+            suma += numero;
+        }
+        float promedio = suma/lista.size();
+        return promedio;
     }
 }
