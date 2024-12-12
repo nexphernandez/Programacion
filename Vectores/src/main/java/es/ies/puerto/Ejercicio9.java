8e2da6c095c02fdc4e7bad91f245e9e501a16631
nexphernandez <nicolaseh3b1112@gmail.com>
2024-12-12 00:57:37 +0000

    Ejercicio 9 realizado

--- Vectores/src/main/java/es/ies/puerto/Ejercicio9.java
+++ Vectores/src/main/java/es/ies/puerto/Ejercicio9.java
@@ -1,4 +1,10 @@
 package es.ies.puerto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  * @author nexphernandez
  * @version 1.0.0
@@ -9,7 +15,27 @@ package es.ies.puerto;
  * Resultado esperado: Entrada: 1, 2, 3, 0. Vector: [1, 2, 3].
  */
 public class Ejercicio9 {
+    static Vector<Integer> numeros = new Vector<>();
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Inserte numeros para hacer la lista, el numero 0 termina la lista:");
+        crearVectorDinamico(numeros);
+        System.out.println(numeros);
+    }
+    /**
+     * Funcion para crear una lista dinamica
+     * @param lista a crear
+     * @return lista creada
+     */
+    public static List<Integer> crearVectorDinamico(List<Integer> lista){
+        if (lista == null) {
+            return new ArrayList<>();
+        }
+        int numero = sc.nextInt();
+        while (numero != 0) {
+            lista.add(numero);
+            numero = sc.nextInt();
+        }
+        return lista;
     }
 }
