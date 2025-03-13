897e92a6443bd363bdb13c8c26d132a133532421
nexphernandez <nicolaseh3b1112@gmail.com>
2025-03-13 18:40:03 +0000

    update

--- primera-app-fx/src/main/java/es/ies/puerto/controller/RecuperarContraseniaController.java
+++ primera-app-fx/src/main/java/es/ies/puerto/controller/RecuperarContraseniaController.java
@@ -3,14 +3,22 @@ package es.ies.puerto.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class RecuperarContraseniaController {
-    @FXML TextField textFieldEmailRecover;
     
+    @FXML TextField textFieldEmailRecover;
+    @FXML Text textFieldMensajeRecover;
+    
+    @FXML Button buttonRegistrarEmail;
 
 
     @FXML
     protected void onRecoverButtonClick(){
-
+        if (textFieldEmailRecover == null || textFieldEmailRecover.getText().isEmpty()) {
+            textFieldMensajeRecover.setText("¡El Email no puede ser nulo o vacio!");
+            return;
+        }
+        textFieldMensajeRecover.setText("¡Mensaje enviado!, revisa el email");
     }
 }
